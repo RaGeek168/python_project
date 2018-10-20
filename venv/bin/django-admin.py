85d23ec459ad9376b4900c87ceb9b1c40e7941e3
theara_ouen <theara.ouen777@gmail.com>
2018-10-20 16:04:56 +0700

    Initial commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/theara_ouen/01.SystemRealWorld/RealEstateProject/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
